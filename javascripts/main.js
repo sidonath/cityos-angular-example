09b2c4032e79902168d0617825fc0c0a30aca1e4
Damir Zekic <damirz@gmail.com>
2015-03-06 13:34:45 +0100

    Connect to a WebSocket server and log data

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,17 @@
-console.log("Hello, world!");
+var ws = new WebSocket('ws://localhost:9090');
+
+ws.onopen = function (event) {
+  console.log('Hurrah! Connected to the WebSocket server!');
+};
+
+ws.onmessage = function (event) {
+  console.log('The server told us: %o', event.data);
+};
+
+ws.onerror = function (event) {
+  console.log('Ouch :(');
+}
+
+ws.onclose = function (event) {
+  console.log('Server closed the connection. What to do now?');
+}
