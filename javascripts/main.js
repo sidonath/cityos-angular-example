bcb70e17a7b21b81c714f91b8c0fdf1be93116b7
Damir Zekic <damirz@gmail.com>
2015-03-08 11:27:23 +0100

    Plot data on a chart using Highcharts

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,7 +1,16 @@
 var dataVis = angular.module('dataVis', ['ngWebSocket']);
+var chart;
 
 dataVis.controller('ListController', function($scope, $websocket) {
   $scope.data = [];
+
+  $scope.$watchCollection('data', function(newData) {
+    if (chart) {
+      // Highcharts has a nasty habit of mutating arrays given to it, so we'll give it a copy
+      newData = newData.slice();
+      chart.series[0].setData(newData);
+    }
+  });
 
   var ws = $websocket('ws://localhost:9090');
 
@@ -11,7 +20,8 @@ dataVis.controller('ListController', function($scope, $websocket) {
 
   ws.onMessage(function(event) {
     console.log('The server told us: %o', event.data);
-    $scope.data.push(event.data);
+    var data = Number(event.data);
+    $scope.data.push(data);
   });
 
   ws.onError(function (event) {
@@ -20,5 +30,28 @@ dataVis.controller('ListController', function($scope, $websocket) {
 
   ws.onClose(function (event) {
     console.log('Server closed the connection. What to do now?');
+  });
+});
+
+$(function() {
+  chart = new Highcharts.Chart({
+    chart: {
+      type: 'column',
+      renderTo: 'chart'
+    },
+    title: {
+      text: 'Flux Capacitor Oscillations per Second'
+    },
+    xAxis: {
+      title: {
+        text: 'Second'
+      }
+    },
+    yAxis: {
+      title: {
+        text: 'Oscillations per Second'
+      }
+    },
+    series: [{ data: [] }]
   });
 });
